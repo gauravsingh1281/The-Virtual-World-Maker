d3d0101bf0b3ac11ba15e234483bf935f0bb4f0e
Gaurav Pratap Singh <gs492979@gmail.com>
2025-07-11 23:06:58 +0530

    tailwind added and removed uneccessary files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
   return (
     <>
       <h1>The Virtual World Maker</h1>
+      <p>A Internet Company</p>
     </>
   );
 }
